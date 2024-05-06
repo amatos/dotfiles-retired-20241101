2508b8ab205c99298e3f49932da1bfbad2f81f1f
Alberth Matos <alberth@matos.cc>
2024-05-05 23:41:24 -0400

    Update .local/bin/bootstrap-fedora.sh

--- dot_local/bin/executable_bootstrap-fedora.sh
+++ dot_local/bin/executable_bootstrap-fedora.sh
@@ -2,17 +2,19 @@
 
 ### Add  Packages
 # Add Starship
-yes | sudo dnf copr enable atim/starship 2>&1
+yes | sudo dnf copr enable atim/starship > /dev/null
 sudo dnf install -y starship
 
 # Add CoolerControl
 sudo dnf install dnf-plugins-core
-yes | sudo dnf copr enable codifryed/CoolerControl
+yes | sudo dnf copr enable codifryed/CoolerControl > /dev/null
 sudo dnf install -y coolercontrol
 sudo systemctl enable --now coolercontrold
 
 # Add Obsidian
-flatpak install -y --noninteractive --system md.obsidian.Obsidian
+sudo dnf install fedora-flathub-remote.noarch
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install -y --noninteractive --system md.obsidian.Obsidian
 # Create Obsidian directory if required
 if [ ! -e "$HOME/Obsidian" ]; then
     mkdir "$HOME/Obsidian"
@@ -26,6 +28,12 @@ pushd ~/Downloads || exit
 curl https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-2.3.1.31116.tar.gz -o jetbrains-toolbox.tar.gz
 tar xfvz jetbrains-toolbox.tar.gz
 popd || return
+
+# Add Cockpit
+sudo dnf install -y cockpit
+
+# Add Chezmoi
+sudo dnf install https://github.com/twpayne/chezmoi/releases/download/v2.48.0/chezmoi-2.48.0-x86_64.rpm
 
 ### Start systemd entries
 sudo systemctl enable cockpit.socket
