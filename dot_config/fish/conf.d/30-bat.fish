f45ff310ff8960c42fbe0e8a61afea0e8760a34f
Alberth Matos <alberth@matos.cc>
2024-10-21 13:19:47 -0400

    Update .config/fish/conf.d/30-bat.fish

--- dot_config/fish/conf.d/30-bat.fish
+++ dot_config/fish/conf.d/30-bat.fish
@@ -3,7 +3,7 @@
 if [ -e /usr/bin/bat ]
     set -gx BATDIFF_USE_DELTA true
     function cat --wraps bat
-        bat --style plain $argv
+        bat --paging=never --style plain $argv
     end
     abbr -a B --position anywhere --set-cursor "% | bat"
     #    abbr -a --position anywhere -- -h "-h 2>&1 | bat --plain --language=help"
