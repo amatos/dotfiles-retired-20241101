4551b8debfa0f1466de9595683a339884881e5d2
Alberth Matos <alberth@matos.cc>
2024-10-24 14:59:55 -0400

    Add .local/share/dark-mode.d/bat.sh Add .local/share/dark-mode.d/starship.sh

---
+++ dot_local/share/dark-mode.d/executable_bat.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm ~/.config/bat/config
+ln -s ~/.config/bat/config.dark ~/.config/bat/config
---
+++ dot_local/share/dark-mode.d/executable_starship.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm ~/.config/starship.toml
+ln -s ~/.config/starship-dark.toml ~/.config/starship.toml
