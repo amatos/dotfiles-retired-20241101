d2a4e7623b7e42481913b1874521e7e79cb071d0
Alberth Matos <alberth@matos.cc>
2024-10-24 15:00:51 -0400

    Add .local/share/light-mode.d/bat.sh Add .local/share/light-mode.d/starship.sh

---
+++ dot_local/share/light-mode.d/executable_bat.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm ~/.config/bat/config
+ln -s ~/.config/bat/config.light ~/.config/bat/config
---
+++ dot_local/share/light-mode.d/executable_starship.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm ~/.config/starship.toml
+ln -s ~/.config/starship-light.toml ~/.config/starship.toml
