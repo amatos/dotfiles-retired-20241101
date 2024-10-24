9cd8149854621ff24a41bae20deaedf60aec219d
Alberth Matos <alberth@matos.cc>
2024-10-24 12:35:50 -0400

    Add .local/share/dark-mode.d/gtk.sh Add .local/share/dark-mode.d/kitty.sh Change attributes of .local/share/dark-mode.d/kde-global-theme.sh Change attributes of .local/share/dark-mode.d/kde-gtk-theme.sh Change attributes of .local/share/dark-mode.d/kde-konsole-theme.sh Change attributes of .local/share/dark-mode.d/kde-wallpaper.sh

---
+++ dot_local/share/dark-mode.d/executable_gtk.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.desktop.interface gtk-theme "adwaita dark"   # for GTK3 apps
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"   # for GTK4 apps
+
