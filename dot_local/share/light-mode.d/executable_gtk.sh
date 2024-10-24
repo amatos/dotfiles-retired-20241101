f191757787bc35d74c581a4e1dc852596e43083f
Alberth Matos <alberth@matos.cc>
2024-10-24 12:35:59 -0400

    Add .local/share/light-mode.d/gtk.sh Add .local/share/light-mode.d/kitty.sh Change attributes of .local/share/light-mode.d/kde-global-theme.sh Change attributes of .local/share/light-mode.d/kde-gtk-theme.sh Change attributes of .local/share/light-mode.d/kde-konsole-theme.sh Change attributes of .local/share/light-mode.d/kde-wallpaper.sh Change attributes of .local/share/light-mode.d/kde-yakuake-theme.sh

---
+++ dot_local/share/light-mode.d/executable_gtk.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.desktop.interface gtk-theme "adwaita"   # for GTK3 apps
+gsettings set org.gnome.desktop.interface color-scheme "prefer-light"   # for GTK4 apps
+
